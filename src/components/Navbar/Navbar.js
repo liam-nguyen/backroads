8ac5715581acb21a6be6ec6336787b80a6559ec4
liam-nguyen <nguyen.dch@gmail.com>
2020-06-20 16:04:05 -0700

    Add styled links to NavBar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+import { FaAlignRight } from "react-icons/fa"
+import { Link } from "gatsby"
+import links from "../../constants/links"
+import socialIcons from "../../constants/social-links"
+
+import styles from "./navbar.module.css"
+import logo from "../../images/logo.svg"
+
+export default function Navbar() {
+  const [isOpen, setNav] = useState(false)
+
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen)
+  }
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <img src={logo} alt="backroads logo" />
+          <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+            <FaAlignRight className={styles.logoIcon} />
+          </button>
+        </div>
+
+        <ul
+          className={
+            isOpen
+              ? `${styles.navLinks} ${styles.showNav}`
+              : `${styles.navLinks}`
+          }
+        >
+          {links.map(({ path, text }, index) => (
+            <li key={index}>{<Link to={path}>{text}</Link>}</li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
