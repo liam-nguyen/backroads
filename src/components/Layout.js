46f9d150a0cccd745661ba4353970cace0a48136
liam-nguyen <nguyen.dch@gmail.com>
2020-06-20 17:19:40 -0700

    Fix Layout's css to push Footer to the bottom

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,14 +2,14 @@ import React from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 
-import "./layout.css"
+import "../css/layout.css"
 
 export default function Layout({ children }) {
   return (
-    <>
+    <main>
       <Navbar />
       {children}
       <Footer />
-    </>
+    </main>
   )
 }
