f72401bb1a40802a89bb4062265009a1fba691e6
liam-nguyen <nguyen.dch@gmail.com>
2020-06-20 17:05:32 -0700

    Add Banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/Layout"
 import SimpleHero from "../components/SimpleHero/SimpleHero"
+import Banner from "../components/Banner/Banner"
 
 export default function Home() {
   return (
     <Layout>
-      <SimpleHero />
+      <SimpleHero>
+        <Banner title="Liam" info="haha">
+          <Link className="btn-white" to="/tours">
+            Explore tours
+          </Link>
+        </Banner>
+      </SimpleHero>
     </Layout>
   )
 }
