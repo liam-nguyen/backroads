5f7895856c470e2e7d5d6d772c1ab9bb303c01ad
liam-nguyen <nguyen.dch@gmail.com>
2020-06-21 15:01:54 -0700

    Replace SimpleHero to StyledHero in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/Layout"
-import SimpleHero from "../components/SimpleHero"
 import Banner from "../components/Banner"
 import About from "../components/Home/About"
 import Services from "../components/Home/Services"
+import StyledHero from "../components/StyledHero"
 
 export default function Home() {
+  const { heroImage } = useStaticQuery(heroImageQuery)
   return (
     <Layout>
-      <SimpleHero>
+      <StyledHero home image={heroImage.childImageSharp.fluid}>
         <Banner title="Continue Exploring" info="An adventure is waiting ahead">
           <Link className="btn-white" to="/tours">
             Explore tours
           </Link>
         </Banner>
-      </SimpleHero>
+      </StyledHero>
       <About />
       <Services />
     </Layout>
   )
 }
+
+const heroImageQuery = graphql`
+  {
+    heroImage: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 4160, quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
