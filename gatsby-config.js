08c3199d006bd75e9ad86cbdd7ee5d1f84f726b6
liam-nguyen <nguyen.dch@gmail.com>
2020-06-20 19:43:43 -0700

    Add metadata to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    title: "BackRoads",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+    author: "Liam Nguyen",
+  },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-styled-components"],
 }
